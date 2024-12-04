e52989ea867d685065e9b73800ebed7b7324b408
Christian Ganhör <54410868+Tigxy@users.noreply.github.com>
2024-12-04 21:26:16 +0100

    Update events.js

--- lj_calendar/events.js
+++ lj_calendar/events.js
@@ -1,5 +1,6 @@
 const events = [
-  { title: 'Weihnachts-BEZI-Update', startDate: '17.12.2024', description: '' },
-  { title: 'Kongress', startDate: '03.01.2024', endDate: '05.01.2024', description: 'Ried im Innkreis' },
-  { title: 'Lange Nacht der Landjugend', startDate: '04.01.2024', description: 'Ried im Innkreis Messehalle' },
+  { title: 'Weihnachts-BEZI-Update', startDate: '17.12.2025', description: '' },
+  { title: 'Kongress', startDate: '03.01.2025', endDate: '05.01.2025', description: 'Ried im Innkreis' },
+  { title: 'Lange Nacht der Landjugend', startDate: '04.01.2025', description: 'Ried im Innkreis Messehalle' },
+  { title: 'eljot', startDate: '26.07.2025', description: 'Ort im Innkreis' },
 ];
